0ec9fc589f2b1e7a76ea03e392d88090904fc681
christoph bähr <christoph.baehr@live.com>
2022-09-13 18:47:29 +0200

    Placed Pins can now be followed to linked Notes

--- src/main/java/de/macniel/campaignwriter/editors/MapNoteEditor.java
+++ src/main/java/de/macniel/campaignwriter/editors/MapNoteEditor.java
@@ -3,23 +3,27 @@ package de.macniel.campaignwriter.editors;
 import com.google.gson.Gson;
 import de.macniel.campaignwriter.Note;
 import de.macniel.campaignwriter.NoteType;
+import javafx.beans.property.Property;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.effect.ColorInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import javafx.util.Callback;
+import org.controlsfx.control.PropertySheet;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class MapNoteEditor implements EditorPlugin {
     private Callback<String, Note> onNoteRequest;
@@ -31,8 +35,17 @@ public class MapNoteEditor implements EditorPlugin {
 
     private Gson gsonParser;
 
+    private MapPin selectedPin;
+
     private MapNoteDefinition noteStructure;
     private Pane root;
+    private PropertySheet mapProperties;
+    private TextField labelProp;
+    private ColorPicker colorProp;
+    private ComboBox<Note> noteReferenceProp;
+
+    private Button deletePinButton;
+    private VBox mapPropertiesPane;
 
     @Override
     public NoteType defineHandler() {
@@ -75,6 +88,7 @@ public class MapNoteEditor implements EditorPlugin {
             backgroundLayer.imageProperty().set(new Image(noteStructure.backgroundPath));
             viewer.setScaleZ(noteStructure.zoomFactor);
 
+
             if (noteStructure.pins == null) {
                 noteStructure.pins = new ArrayList<>();
             }
@@ -84,6 +98,7 @@ public class MapNoteEditor implements EditorPlugin {
 
     @Override
     public Node defineEditor() {
+        BorderPane bp = new BorderPane();
         viewer = new ScrollPane();
         root = new Pane();
         backgroundLayer = new ImageView();
@@ -100,11 +115,107 @@ public class MapNoteEditor implements EditorPlugin {
                 n.label = "Neuer Pin";
                 n.noteReference = null;
                 noteStructure.pins.add(n);
+                selectedPin = n;
                 renderPin(n);
+                updateEditor();
+            } else {
+                System.out.println("Root clicked");
+                selectedPin = null;
+                mapPropertiesPane.setVisible(false);
             }
         });
 
-        return viewer;
+        mapPropertiesPane = new VBox();
+
+        labelProp = new TextField();
+        colorProp = new ColorPicker();
+        noteReferenceProp = new ComboBox<Note>();
+        deletePinButton = new Button("Pin entfernen");
+
+
+        mapPropertiesPane.getChildren().add(new Label("Beschriftung"));
+        mapPropertiesPane.getChildren().add(labelProp);
+
+        labelProp.onActionProperty().set(e -> {
+            if (selectedPin != null) {
+                selectedPin.label = labelProp.getText();
+                refreshView();
+            }
+
+        });
+
+        mapPropertiesPane.getChildren().add(new Label("Farbe"));
+        mapPropertiesPane.getChildren().add(colorProp);
+
+        mapPropertiesPane.getChildren().add(new Label("Verknüpfte Notiz"));
+        mapPropertiesPane.getChildren().add(noteReferenceProp);
+
+        noteReferenceProp.onActionProperty().set(e -> {
+            Note selected = noteReferenceProp.getValue();
+            if (selectedPin != null && selected != null) {
+                selectedPin.noteReference = selected.reference;
+                refreshView();
+            }
+        });
+
+
+        mapPropertiesPane.getChildren().add(new Separator());
+
+        mapPropertiesPane.getChildren().add(deletePinButton);
+
+        deletePinButton.onActionProperty().set(e -> {
+            if (selectedPin != null) {
+                noteStructure.pins.remove(selectedPin);
+                selectedPin = null;
+                refreshView();
+                mapPropertiesPane.setVisible(false);
+            }
+        });
+
+        bp.setRight(mapPropertiesPane);
+        bp.setCenter(viewer);
+
+        populateNoteReferenceProp();
+
+        return bp;
+    }
+
+    void populateNoteReferenceProp() {
+        ArrayList<Note> notes = Note.getAll(); // TODO: this should not be possible, request maincontroller instead
+        noteReferenceProp.setItems(FXCollections.observableArrayList(notes));
+
+        Callback<ListView<Note>, ListCell<Note>> itemCellFactory = new Callback<ListView<Note>, ListCell<Note>>() {
+            @Override
+            public ListCell<Note> call(ListView<Note> param) {
+                return new ListCell<Note>() {
+
+                    @Override
+                    protected void updateItem(Note item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item == null || empty) {
+                            setGraphic(null);
+                        } else {
+                            setText(item.getLabel());
+                        }
+                    }
+                } ;
+            }
+        };
+
+        noteReferenceProp.setButtonCell(itemCellFactory.call(null));
+        noteReferenceProp.setCellFactory(itemCellFactory);
+
+    }
+
+    void updateEditor() {
+        populateNoteReferenceProp();
+
+        labelProp.setText(selectedPin.label);
+
+        if (selectedPin.noteReference != null) {
+            noteReferenceProp.getSelectionModel().select(onNoteRequest.call(selectedPin.noteReference.toString()));
+        }
+        mapPropertiesPane.setVisible(true);
     }
 
     void renderPin(MapPin pin) {
@@ -116,8 +227,24 @@ public class MapNoteEditor implements EditorPlugin {
         pinButton.setLayoutY(pin.y - (pinSize/2));
         pinButton.setPrefWidth(pinSize);
         pinButton.setPrefHeight(pinSize);
+        pinButton.setCursor(Cursor.HAND);
+
         Tooltip p = new Tooltip();
         p.setText(pin.label);
+
+
+        pinButton.onMouseClickedProperty().set(e -> {
+            if (e.getButton() == MouseButton.SECONDARY) {
+                selectedPin = pin;
+                updateEditor();
+                System.out.println("Pin rightclicked");
+            } else {
+                if (pin.noteReference != null) {
+                    onNoteLoadRequest.call(pin.noteReference.toString());
+                }
+                mapPropertiesPane.setVisible(false);
+            }
+        });
         pinButton.setTooltip(p);
         root.getChildren().add(pinButton);
     }
