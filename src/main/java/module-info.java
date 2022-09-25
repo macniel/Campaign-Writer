716ff5c6d62296e6656dc925ac291ee36693967b
Christoph Bähr <christoph.baehr@live.com>
2022-09-25 18:05:43 +0200

    Add transitiveness and first test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module de.macniel.campaignwriter {
-    requires javafx.controls;
-    requires javafx.fxml;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
